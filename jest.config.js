3214b79ec5cb6c8c6f6a070d5d7a3f560b7706e5
louis <louis@omos.com>
2021-08-18 10:48:28 +0800

    init

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
+  preset: '@vue/cli-plugin-unit-jest',
   transform: {
-    "^.+\\.vue$": "vue-jest",
+    '^.+\\.vue$': 'vue-jest'
   },
-};
+  moduleNameMapper: {
+    // 模塊別名配置
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/tests/unit/coverage',
+  collectCoverageFrom: ['!**/node_modules/**', '!**/vendor/**']
+}
